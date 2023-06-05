135979d110895898a2a45da96226277cfb34fa49
Antônio Lucas C Araújo <112925082+UmJovemProgramador@users.noreply.github.com>
2023-06-05 08:29:25 -0300

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -5,7 +5,7 @@ inputQuestion.addEventListener("keypress", (e) => {
   if (inputQuestion.value && e.key === "Enter") SendQuestion();
 });
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 
 function SendQuestion() {
   var sQuestion = inputQuestion.value;
